493d7ca6d8aa3841279509de137d19c3faabbfa7
jaeheng <jaeheng@126.com>
2020-03-14 10:07:16 +0800

    更新v2.6.1下载链接

--- page/product.php
+++ page/product.php
@@ -10,9 +10,9 @@ if (!defined('EMLOG_ROOT')) {
     <div class="jumbotron" style="background-color: #07111B;">
         <div class="container">
             <h2 class="title">Beginning for Emlog</h2>
-            <p class="slogan">简洁大方，多种布局可供选择,非常适合个人博客,兼容官方Emlog6.0.0 <br />最新更新日期: 2020/03/13 </p>
-            <a href="https://pan.baidu.com/s/1YzDtpv4gUTANBsTVI4SQPg" target="_blank" class="btn btn-blue">立即下载</a>
-            <span class="version">当前版本 v2.6</span>
+            <p class="slogan">简洁大方，多种布局可供选择,非常适合个人博客,兼容官方Emlog6.0.0 <br />最新更新日期: 2020/03/14 </p>
+            <a href="https://pan.baidu.com/s/1Qv9XOPxpfWv5EoX4gsPjLg" target="_blank" class="btn btn-blue">立即下载</a>
+            <span class="version">当前版本:v2.6.1 提取码:78ke</span>
             <img src="<?php echo TEMPLATE_URL;?>static/images/dna.svg" data-src="<?php echo TEMPLATE_URL?>/preview.jpg" class="preview lazyload" style="background: #fff;">
         </div>
     </div>
